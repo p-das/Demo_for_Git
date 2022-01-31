fcb26e9f74bb8186738bc397fcaece5de297db62
ParamitaDas <xxxx>
2022-01-31 15:12:50 +0530

    removedbatch file

--- demojob.bat
+++
@@ -1,3 +0,0 @@
-cd\
-cd C:\Users\edaspta\Desktop\Python_All_in_One\Learning_From_Github
-C:\Users\edaspta\PycharmProjects\Python36\python.exe C:\Users\edaspta\Desktop\Python_All_in_One\Learning_From_Github\demo.py
